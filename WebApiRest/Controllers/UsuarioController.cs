6bf50fff8314bc1a9666f128859661febb95b114
rrarangel <rrarangel@gmail.com>
2019-02-22 16:15:17 -0300

    Add project files.

---
+++ WebApiRest/Controllers/UsuarioController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiRest.Models;
+
+namespace WebApiRest.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        private static List<Usuario> listUser = new List<Usuario>();
+
+        
+        [AcceptVerbs("POST")]
+        [Route("CadastrarUsuario")]
+        public string CadastrarUsuario(Usuario u)
+        {
+            listUser.Add(u);
+
+            return "Usuário cadastrado com sucesso!";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarUsuarioPorCodigo/{codigo}")]
+        public Usuario ConsultaUsuarioPorCodigo(int codigo)
+        {
+            return
+                listUser.Where(u => u.codigo == codigo).Select(u => u).FirstOrDefault();
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("ExcluirUsuario/{codigo}")]
+        public string ExcluirUsuario(int codigo)
+        {
+                listUser.Remove(listUser.Where(u => u.codigo == codigo).Select(u => u).FirstOrDefault());
+            
+            return "Registro excluido com sucesso!";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("AlterarUsuario")]
+        public string AlterarUsuario(Usuario usuario)
+        {
+            listUser.Where(u => u.codigo == usuario.codigo)
+                         .Select(s =>
+                         {
+                             s.codigo = usuario.codigo;
+                             s.login = usuario.login;
+                             s.nome = usuario.nome;
+
+                             return s;
+
+                         }).ToList();
+            
+            return "Usuário alterado com sucesso!";
+        }
+
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarUsuarios")]
+        public List<Usuario> ConsultarUsuarios()
+        {
+            return listUser;
+        }
+
+    }
+}
---
+++ WebApiRest/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiRest.Models
+{
+    public class Usuario
+    {
+        public int codigo { get; set; }
+        public string nome { get; set; }
+        public string login { get; set; }
+
+        public Usuario(int codigo, string nome, string login)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.login = login;
+        }
+
+
+    }
+}
